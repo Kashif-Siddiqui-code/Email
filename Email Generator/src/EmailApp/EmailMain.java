5c0e8cf971c38d6d0afef1fd8609d7ce0f0fd4a4
KASHIF SIDDIQUI <KASHIF SIDDIQUI@DESKTOP-SM4RA3F>
2021-03-04 22:55:03 -0500

    EMAIL

---
+++ Email Generator/src/EmailApp/EmailMain.java
@@ -0,0 +1,13 @@
+package EmailApp;
+
+import EmailApp.Email;
+
+public class EmailMain {
+
+		public static void main(String[] args) {
+			Email em1 = new Email("Kashif","Siddiqui");
+			System.out.println(em1.showInfo());
+		}
+
+
+}
